641f29cdeb3604baa08752959ed0f66a6e68ef3d
AntFelipe <felipe1mesquita2@gmail.com>
2025-08-06 20:34:14 +0000

    Alterações necessárias e correção de código

--- Nivel_Mestre/DesafioMESTRE.c
+++ Nivel_Mestre/DesafioMESTRE.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+    #include <stdio.h>
     #define TamanhoNavio 3
     #define LinhaPODER 3
     #define ColunaPODER 3
@@ -20,6 +20,26 @@ int main (){
             tabuleiro[i][j] = 0; // Atribui 0 (água) a todos os pontos
         }
     }
+
+
+    for (int Navio1 = 1, i = 1; Navio1 <= TamanhoNavio; Navio1++){
+        tabuleiro[Navio1][i] = 3; //ele começa na linha 2 e vai somando até ficar 4 - Vertical
+    }
+
+    for (int Navio2_X = 1, i = 8; Navio2_X <= TamanhoNavio; Navio2_X++){
+        tabuleiro[i][Navio2_X] = 3; //ele começa na coluna 2 e vai somando até fica 4 - Horizontal
+    }
+
+    for (int Navio3 = 1, i = 5; Navio3 <= TamanhoNavio; Navio3++){
+        tabuleiro[Navio3+i][Navio3+i] = 3; //ele começa na linha/coluna 7 e vai somando até navio3=3
+    }
+
+    for (int Navio4 = 1, i = 5, j = 0; Navio4 <= TamanhoNavio; Navio4++){
+        tabuleiro[Navio4][Navio4+i-j] = 3; //linha 2
+        j = j + 2; /* vai subtraindo 1 da coluna, já que normalmente ela vai somando 1 (navio4++) e aqui estamos
+        subtraindo 2 (1 - 2= -1) */
+    }
+
 
 // CONFIGURAR E ADICIONAR A TABELA CONE AO TABULEIRO
     for (int i = 0; i < LinhaPODER; i++){ //todas as posições virar 0
@@ -47,7 +67,7 @@ for (int i = 0; i < LinhaPODER; i++) { //controla as linhas
             int colunaTab = colunaInicial + j; //2 à 6
 
             if (linhaTab < 10 && colunaTab < 10) { //passa por cada linha/coluna da Matriz principal. Começa: [2][2]
-                if (PoderCone[i][j] == 5) {
+                if (PoderCone[i][j] == 5 && tabuleiro[linhaTab][colunaTab] != 3) {
                     tabuleiro[linhaTab][colunaTab] = 5; //se for 5 ele muda na matriz principal
                 }
             }
@@ -77,7 +97,7 @@ for (int i = 0; i < LinhaPODER; i++) { //controla as linhas
             int colunaTab = colunaCruz + j; // VAI DE 6 À 8  - IDEM
 
             if (linhaTab < 10 && colunaTab < 10) {
-                if (PoderCone[i][j] == 5) {  //VERIFICA TODA A ÁREA DA TABELA DO CONE, SE FOR IGUAL Á 5 ELE PÕE
+                if (PoderCruz[i][j] == 5 && tabuleiro[linhaTab][colunaTab] != 3) {  //VERIFICA TODA A ÁREA DA TABELA DO CONE, SE FOR IGUAL Á 5 ELE PÕE
                     tabuleiro[linhaTab][colunaTab] = 5; //NO TABULEIRO
                 }
             }
@@ -108,34 +128,16 @@ for (int i = 0; i < LinhaPODER_octa; i++){
 for (int i = 0; i < LinhaPODER_octa; i++){
     int LINHAINICIAL_octa = 5, COLUNAINICIAL_octa = 1;
     for(int j = 0; j < ColunaPODER_octa; j++){
-        int TAB_inicialLINHA = LINHAINICIAL_octa + i;
-        int TAB_inicialCOLUNA = COLUNAINICIAL_octa + j;
+        int linhaTab = LINHAINICIAL_octa + i;
+        int colunaTab = COLUNAINICIAL_octa + j;
 
-        if (TAB_inicialLINHA < 10 && TAB_inicialCOLUNA < 10){
-            if (PoderOctaedro[i][j] == 5){
-                tabuleiro[TAB_inicialLINHA][TAB_inicialCOLUNA] = 5;
+        if (linhaTab < 10 && colunaTab < 10){
+            if (PoderOctaedro[i][j] == 5 && tabuleiro[linhaTab][colunaTab] != 3){
+                tabuleiro[linhaTab][colunaTab] = 5;
             }
         }
     }
 }
-
-    for (int Navio1 = 1, i = 1; Navio1 <= TamanhoNavio; Navio1++){
-        tabuleiro[Navio1][i] = 3; //ele começa na linha 2 e vai somando até ficar 4 - Vertical
-    }
-
-    for (int Navio2_X = 1, i = 8; Navio2_X <= TamanhoNavio; Navio2_X++){
-        tabuleiro[i][Navio2_X] = 3; //ele começa na coluna 2 e vai somando até fica 4 - Horizontal
-    }
-
-    for (int Navio3 = 1, i = 5; Navio3 <= TamanhoNavio; Navio3++){
-        tabuleiro[Navio3+i][Navio3+i] = 3; //ele começa na linha/coluna 7 e vai somando até navio3=3
-    }
-
-    for (int Navio4 = 1, i = 5, j = 0; Navio4 <= TamanhoNavio; Navio4++){
-        tabuleiro[Navio4][Navio4+i-j] = 3; //linha 2
-        j = j + 2; /* vai subtraindo 1 da coluna, já que normalmente ela vai somando 1 (navio4++) e aqui estamos
-        subtraindo 2 (1 - 2= -1) */
-    }
 
 int INDEX_1; //linhas
 int INDEX_0; //colunas
@@ -151,5 +153,5 @@ int INDEX_0; //colunas
         };
         printf("\n");
 
-    };
+    }
 }
